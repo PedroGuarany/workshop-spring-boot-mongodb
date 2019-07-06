c744addfc156e2783349b184256b04171c72431e
PedroGuarany <pedrogt.alu.lmb@gmail.com>
2019-07-06 18:06:24 -0300

    Usuario por id

--- src/main/java/com/pedroguarany/workshopmongo/services/UserService.java
+++ src/main/java/com/pedroguarany/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.pedroguarany.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pedroguarany.workshopmongo.domain.User;
 import com.pedroguarany.workshopmongo.repository.UserRepository;
+import com.pedroguarany.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
-	}	
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
